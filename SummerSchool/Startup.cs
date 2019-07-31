3897bebf6f32e77b6ac33ac2724912d668e3bff2
Slavchanyk <o.slavchanyk@gmail.com>
2019-07-31 14:44:45 +0300

    Add project files.

---
+++ SummerSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SummerSchool.Startup))]
+namespace SummerSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
